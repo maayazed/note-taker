82e207d65ffea04f47ad3af47978894e00199bb9
maayazed <mayahzebrasky@gmail.com>
2021-07-02 14:07:13 -0500

    deploy fix 4

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ app.use(express.json());
 
 // Routes
 app.get('/', function(req, res) {
-    res.json(path.join(__dirname, 'public/index.html'));
+    res.sendFile(path.join(__dirname, 'public/index.html'));
 }); 
 
 app.get('/notes', function(req, res) {
-    res.json(path.join(__dirname, 'public/notes.html'));
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
 // Start server
