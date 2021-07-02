6e5436feb47f264a4e898fcfb8b4701e0bd64699
maayazed <mayahzebrasky@gmail.com>
2021-07-02 13:48:45 -0500

    fixed file locations for heroku to detect script language

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Routes
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../Develop/public/index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '../Develop/public/notes.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
 
 // Start server
 
