2cb2a576070fa01fffde644c03d482df4edd52d5
maayazed <mayahzebrasky@gmail.com>
2021-07-05 14:17:55 -0500

    route fix (again)

--- db/routes.js
+++ db/routes.js
@@ -1,14 +1,18 @@
 const fs = require('fs');
 const path = require('path');
 
-module.exports = app => {
-    // VIEW
+module.exports = (app) => {
+    
+    app.get('/notes', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/notes.html'));
+    });
 
-    app.get('', function(req, res) {
+    app.get('', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/index.html'));
     });
 
-    app.get('/notes', function(req, res) {
-        res.sendFile(path.join(__dirname, '../public/notes.html'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/index.html'));
     });
+
 };
